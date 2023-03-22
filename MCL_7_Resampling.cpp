b7fb0765288b1e74e1c21af94db76bb396fd1301
bmaxdk <choh8uw@gmail.com>
2023-03-21 22:59:47 -0400

    MCL7

--- MCL_7_Resampling.cpp
+++ MCL_7_Resampling.cpp
@@ -9,6 +9,35 @@ double w[] = { 0.6, 1.2, 2.4, 0.6, 1.2 };//You can also change this to a vector
 
 
 
+double sum = 0;
+
+// double *getProb(double w[], int n)
+// {
+//     double *p;
+//     p = new double[n];
+//     for(unsigned int i=0; i<n; i++)
+//         sum += w[i];
+
+
+//     for(unsigned int i=0; i<n; i++)
+//     {
+//         p[i] = w[i]/sum;
+//         cout <<"P"<<i+1<<"="<<p[i]<<endl;      
+//     }
+//     return p;
+// }
+
+void getProb(double w[], int n)
+{
+    for(unsigned int i=0; i<n; i++)
+        sum += w[i];
+
+    for(unsigned int i=0; i<n; i++)
+    {
+        w[i] = w[i]/sum;
+        cout <<"P"<<i+1<<"="<<w[i]<<endl;      
+    }
+}
 
 
 
@@ -18,11 +47,11 @@ int main()
     //P1=Value
     //:
     //P5=Value
-    
-    
-    
-    
-    
+    double *p;
+    int n = sizeof(w)/sizeof(w[0]);
+    // p = getProb(w, n);
+    getProb(w, n); // this case is changing all the w values as well
+
     
     return 0;
 }
